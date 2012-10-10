a601ed8cac80fc6062418d75217b42cb151a53eb
Lucas Garron <lucasg@gmx.de>
2012-10-09 19:18:14 -0700

    Box of maximum volume calculation.

--- src/us/garron/refcon/Test.java
+++ src/us/garron/refcon/Test.java
@@ -13,6 +13,8 @@ public class Test {
 		System.out.println("x: " + x.getValue());
 		System.out.println("y: " + y.getValue());
 
+		////////////////////////////////
+		
 		final Expression a = new PositiveGiven("a", 3);
 		final Expression b = new PositiveGiven("b", 4);
 		
@@ -23,6 +25,8 @@ public class Test {
 		
 		System.out.println("c: " + c.getValue());
 
+		////////////////////////////////
+		
 		final Expression g1 = new Minus(new PositiveInteger(1));
 		final Expression g2 = new Minus(new PositiveInteger(1));
 		final Expression g3 = new PositiveInteger(1);
@@ -30,6 +34,19 @@ public class Test {
 		final Expression goldenRatioSmallNegative = new QuadraticEquationRoot1(g1, g2, g3);
 		
 		System.out.println("Golden ratio: " + goldenRatioSmallNegative.getValue());
+
+		////////////////////////////////
+
+		final Expression w = new PositiveGiven("width", 2);
+		final Expression h = new PositiveGiven("height", 1);
+		
+		final Expression boxOfMaximumVolume = new QuadraticEquationRoot1(
+				new Times (w, h),
+				new Minus(new Times(new PositiveInteger(4), new Plus(w, h))),
+				new PositiveInteger(12)
+		);
+		
+		System.out.println("Value for box of maximum volume: " + boxOfMaximumVolume.getValue());
 		
 	}
 
